4b66b279189ee28d901f79afdd9a7a5f2303ec3d
liubin <bin.liu@travelzen.com>
2015-02-15 18:16:31 +0800

    deminal convers to given

---
+++ data-strusctures-and-algorithms/src/main/java/com/lubby/stack/DeminalConversion.java
@@ -0,0 +1,46 @@
+package com.lubby.stack;
+/**  
+ * 
+ * @author liubin   
+ * @version 创建时间：2015年2月15日 下午5:47:20  
+ * 
+ */
+
+
+public class DeminalConversion {
+	public static void main(String[] args) {
+		
+		String binary = conversion(100L, 2);
+		System.out.println(binary);
+		
+	}	
+	/** conver the deminal to given scale**/
+	private static String conversion(Long deminalNum,int scale){
+		if(scale <= 0 ){
+			throw new RuntimeException("scale can not less than or queal 0");
+		}
+		
+		Stack<Long> stack = new Stack<>();
+		Long num = 0L;
+		
+		while ( (num = deminalNum / scale) != 0) {
+			stack.push(deminalNum % scale);
+			deminalNum = num;
+		}
+		
+		StringBuffer sb = new StringBuffer();
+		
+		if(stack.getSize() != 0 && stack.peek() == 0){
+			stack.pop();
+		}
+		
+		while (stack.getSize() != 0) {
+			sb.append(stack.pop());
+			
+		}
+		
+		
+		return sb.toString();
+	}
+
+}
